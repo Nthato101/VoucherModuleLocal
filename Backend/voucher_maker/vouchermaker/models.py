09c784eb24fd3795220fa1804a3e5f32b27e021c
Nthato <122855450+Nthato101@users.noreply.github.com>
2024-02-28 13:24:24 +0200

    Update models.py

--- Backend/voucher_maker/vouchermaker/models.py
+++ Backend/voucher_maker/vouchermaker/models.py
@@ -7,10 +7,8 @@ class Voucher(models.Model):
     redemptions = models.IntegerField(default=0)
     created_at = models.DateField(auto_now_add=True)
     expires_at = models.DateField(null=True)
-    pin = models.CharField(max_length=4, unique=True)
-    status = models.CharField(choices=[('created', 'Not Used'), ('expired', 'Expired'),
-                                       ('partial_used', 'Partially Redeemed'), ('used', 'Used')],
-                              default='created', max_length=50)
+    pin = models.CharField(max_length=4, unique=True,null=True)
+    status = models.CharField(default='created', max_length=50)
 
     def __str__(self):
         return f'{self.id} - {self.status}'
